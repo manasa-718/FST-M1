05815eea79a5ce7c58ce993d31838ba62095f831
manasa-718 <manasa.malyala@ibm.com>
2025-10-10 14:02:20 +0530

    Add files via upload

---
+++ TestNG/Activity3.java
@@ -0,0 +1,19 @@
+package Activity;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Activity3 {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
